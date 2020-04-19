eb00d4e972444f73ddbfa8ae8ec144e4a5510ef1
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2020-04-18 22:56:11 -0500

    Implemented various arithmetic words

--- src/vocabularies/arithmetic.c
+++ src/vocabularies/arithmetic.c
@@ -7,8 +7,8 @@
 
 void _add(Environment *env) {
   if (env->value_stack->size >= 2) {
-    ArbitraryValue *a = pop_stack(env->value_stack);
     ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
     if ((a->type == longtype) & (b->type == longtype)) {
       long *result = malloc(sizeof(long));
       *result = (*(long*)a->value) + (*(long*)b->value);
@@ -26,44 +26,182 @@ void _add(Environment *env) {
   }
 }
 
+void _sub(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) - (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _mul(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) * (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _div(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) / (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _mod(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) % (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _and(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) & (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _or(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) | (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _rshift(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) >> (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
+void _lshift(Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *b = pop_stack(env->value_stack);
+    ArbitraryValue *a = pop_stack(env->value_stack);
+    if ((a->type == longtype) & (b->type == longtype)) {
+      long *result = malloc(sizeof(long));
+      *result = (*(long*)a->value) >> (*(long*)b->value);
+      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
+      result_val->type = longtype;
+      result_val->dynamic = 1;
+      result_val->value = result;
+      push_stack(env->value_stack, result_val);
+      cleanup_value(a);
+      cleanup_value(b);
+    } else {
+      push_stack(env->value_stack, b);
+      push_stack(env->value_stack, a);
+    }
+  }
+}
+
 void register_arithmetic_words (Environment *env) {
   set_value_dictionary(env->dictionaries[primarydictionary], "+", _add);
+  set_value_dictionary(env->dictionaries[primarydictionary], "-", _sub);
+  set_value_dictionary(env->dictionaries[primarydictionary], "*", _mul);
+  set_value_dictionary(env->dictionaries[primarydictionary], "/", _div);
+  set_value_dictionary(env->dictionaries[primarydictionary], "%", _mod);
+  set_value_dictionary(env->dictionaries[primarydictionary], "&", _and);
+  set_value_dictionary(env->dictionaries[primarydictionary], "|", _or);
+  set_value_dictionary(env->dictionaries[primarydictionary], ">>", _rshift);
+  set_value_dictionary(env->dictionaries[primarydictionary], "<<", _lshift);
 }
-
-/*void _println (Environment *env) {
-  if (env->value_stack->size > 0) {
-    ArbitraryValue *val = pop_stack(env->value_stack);  
-    if ((val->type == stringtype) | (val->type == quotetype)) {
-      printf("%s\n", val->value);
-    } else {
-      printf("%d\n", *(long*)val->value);
-    }
-    cleanup_value(val);
-  }
-}
-
-void _print (Environment *env) {
-  if (env->value_stack->size > 0) {
-    ArbitraryValue *val = pop_stack(env->value_stack);  
-    if ((val->type == stringtype) | (val->type == quotetype)) {
-      printf("%s", val->value);
-    } else {
-      printf("%d", *(long*)val->value);
-    }
-    cleanup_value(val);
-  }
-}
-
-void _readline (Environment *env) {
-  ArbitraryValue *val = malloc(sizeof(ArbitraryValue));
-  val->dynamic = 1;
-  val->type = stringtype;
-  val->value = readline(NULL);
-  push_stack(env->value_stack, val);
-}
-
-void register_io_words (Environment *env) {
-  set_value_dictionary(env->dictionaries[primarydictionary], "println", _println);
-  set_value_dictionary(env->dictionaries[primarydictionary], "print", _print);
-  set_value_dictionary(env->dictionaries[primarydictionary], "readln", _readline);
-}*/
