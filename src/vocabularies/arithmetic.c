4b54dcc53ca00b3317cb1d9ad5948fa6abc212fc
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2020-04-19 19:39:23 -0500

    Reimplemented arithmetic

--- src/vocabularies/arithmetic.c
+++ src/vocabularies/arithmetic.c
@@ -5,13 +5,13 @@
 
 #include "arithmetic.h"
 
-void _add(Environment *env) {
+void _generic_arithmetic_word_long(Environment *env, long (*res)(long, long)) {
   if (env->value_stack->size >= 2) {
     ArbitraryValue *b = pop_stack(env->value_stack);
     ArbitraryValue *a = pop_stack(env->value_stack);
     if ((a->type == longtype) & (b->type == longtype)) {
       long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) + (*(long*)b->value);
+      *result = res((*(long*)a->value),(*(long*)b->value));
       ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
       result_val->type = longtype;
       result_val->dynamic = 1;
@@ -24,174 +24,78 @@ void _add(Environment *env) {
       push_stack(env->value_stack, a);
     }
   }
+}
+
+long add_res (long a, long b) {
+  return a + b;
+}
+
+void _add(Environment *env) {
+  _generic_arithmetic_word_long(env, add_res);
+}
+
+long sub_res (long a, long b) {
+  return a - b;
 }
 
 void _sub(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) - (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, sub_res);
+}
+
+long mul_res (long a, long b) {
+  return a * b;
 }
 
 void _mul(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) * (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, mul_res);
+}
+
+long div_res (long a, long b) {
+  return a / b;
 }
 
 void _div(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) / (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, div_res);
+}
+
+long mod_res (long a, long b) {
+  return a % b;
 }
 
 void _mod(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) % (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, mod_res);
+}
+
+long and_res (long a, long b) {
+  return a & b;
 }
 
 void _and(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) & (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, and_res);
+}
+
+long or_res (long a, long b) {
+  return a | b;
 }
 
 void _or(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) | (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, or_res);
+}
+
+long rshift_res (long a, long b) {
+  return a >> b;
 }
 
 void _rshift(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) >> (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, rshift_res);
+}
+
+long lshift_res (long a, long b) {
+  return a << b;
 }
 
 void _lshift(Environment *env) {
-  if (env->value_stack->size >= 2) {
-    ArbitraryValue *b = pop_stack(env->value_stack);
-    ArbitraryValue *a = pop_stack(env->value_stack);
-    if ((a->type == longtype) & (b->type == longtype)) {
-      long *result = malloc(sizeof(long));
-      *result = (*(long*)a->value) >> (*(long*)b->value);
-      ArbitraryValue *result_val = malloc(sizeof(ArbitraryValue));
-      result_val->type = longtype;
-      result_val->dynamic = 1;
-      result_val->value = result;
-      push_stack(env->value_stack, result_val);
-      cleanup_value(a);
-      cleanup_value(b);
-    } else {
-      push_stack(env->value_stack, b);
-      push_stack(env->value_stack, a);
-    }
-  }
+  _generic_arithmetic_word_long(env, lshift_res);
 }
 
 void register_arithmetic_words (Environment *env) {
