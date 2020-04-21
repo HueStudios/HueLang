e4e22c9d8e53c5424e6d5bffa25db031043b2f90
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2020-04-20 19:40:03 -0500

    Implemented when and do

--- src/vocabularies/flow.c
+++ src/vocabularies/flow.c
@@ -31,7 +31,51 @@ void _if (Environment *env) {
       cleanup_value(det);
     } else {
       push_stack(env->value_stack, det);
+      push_stack(env->value_stack, true_sel);
       push_stack(env->value_stack, false_sel);
+    }
+  }
+}
+
+void _when (Environment *env) {
+  if (env->value_stack->size >= 2) {
+    ArbitraryValue *true_sel = pop_stack(env->value_stack);
+    ArbitraryValue *det = pop_stack(env->value_stack);
+    if (((true_sel->type == stringtype) | (true_sel->type == quotetype)) &
+      (det->type == longtype)) {
+      ArbitraryValue *selected;
+      if (*((long*)det->value)) {
+        selected = true_sel;
+        ArbitraryValue *to_call = malloc(sizeof(ArbitraryValue));
+        to_call->type = symboltype;
+        to_call->dynamic = 1;
+        to_call->value = malloc(sizeof(char) * (strlen(selected->value) + 1));
+        strcpy(to_call->value, selected->value);
+        push_stack(env->execution_stack, to_call);
+      } 
+      cleanup_value(true_sel);
+      cleanup_value(det);
+    } else {
+      push_stack(env->value_stack, det);
+      push_stack(env->value_stack, true_sel);
+    }
+  }
+}
+
+void _do (Environment *env) {
+  if (env->value_stack->size >= 1) {
+    ArbitraryValue *true_sel = pop_stack(env->value_stack);
+    if ((true_sel->type == stringtype) | (true_sel->type == quotetype)) {
+      ArbitraryValue *selected;
+      selected = true_sel;
+      ArbitraryValue *to_call = malloc(sizeof(ArbitraryValue));
+      to_call->type = symboltype;
+      to_call->dynamic = 1;
+      to_call->value = malloc(sizeof(char) * (strlen(selected->value) + 1));
+      strcpy(to_call->value, selected->value);
+      push_stack(env->execution_stack, to_call);
+      cleanup_value(true_sel);
+    } else {
       push_stack(env->value_stack, true_sel);
     }
   }
@@ -39,4 +83,6 @@ void _if (Environment *env) {
 
 void register_flow_words (Environment *env) {
   set_value_dictionary(env->dictionaries[primarydictionary], "if", _if);
+  set_value_dictionary(env->dictionaries[primarydictionary], "when", _when);
+  set_value_dictionary(env->dictionaries[primarydictionary], "do", _do);
 }
