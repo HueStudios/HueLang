e362945e98f7bcfac6e8e842087f2bc8fd9f01a8
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2020-05-21 08:05:38 -0500

    Implemented registration of compound types

--- implementation/src/libhuelang/type.c
+++ implementation/src/libhuelang/type.c
@@ -1,15 +1,24 @@
 #if INTERFACE
 typedef struct Environment Environment;
 #include "environment.h"
-#define ATOMTYPEDEFWORD "atomtypedef"
+#define ATOMTYPEDEFWORD "atomic"
+#define COMPOUNDTYPEDEFWORD "compound"
 #endif
 
 #include "type.h"
 
 void Types_RegisterAtomicType(Environment *env, Word typename, unsigned int size) {
-    Word atomtypedefword = DefinitionTable_TokToWord(env->definition_table, ATOMTYPEDEFWORD);
-    Definition typedefinition;
-    typedefinition.type = atomtypedefword;
-    typedefinition.value.number = size;
-    DefinitionTable_SetDefinition(env->definition_table, typename, typedefinition);
+  Word atomtypedefword = DefinitionTable_TokToWord(env->definition_table, ATOMTYPEDEFWORD);
+  Definition typedefinition;
+  typedefinition.type = atomtypedefword;
+  typedefinition.value.number = size;
+  DefinitionTable_SetDefinition(env->definition_table, typename, typedefinition);
+}
+
+void Types_RegisterCompoundType(Environment *env, Word typename, unsigned int (*type_handler)(Word*)) {
+  Word compoundtypedefword = DefinitionTable_TokToWord(env->definition_table, COMPOUNDTYPEDEFWORD);
+  Definition typedefinition;
+  typedefinition.type = compoundtypedefword;
+  typedefinition.value.pointer = type_handler;
+  DefinitionTable_SetDefinition(env->definition_table, typename, typedefinition);
 }
